f8533adf6b9b27c81ec073f2c106037a28b74da1
Chris Jones <48707994+CIJones96@users.noreply.github.com>
2019-07-15 16:48:43 +0100

    Add files via upload

---
+++ create_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `i7467340`.`smart` (
+  `smart_id` INT NOT NULL,
+  `smart_name` VARCHAR(45) NOT NULL,
+  `smart_info` VARCHAR(45) NOT NULL,
+  `smart_energy` DECIMAL(5,2) NOT NULL,
+  `smart_time` TIME NOT NULL,
+  PRIMARY KEY (`smart_id`));
